e46819c7ccb284087fb72225aa7e2780f3d535be
Radman Hayati <Radman.hayatii@gmail.com>
2025-07-16 19:59:23 +0330

    fix: data conflict constraint fixed

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,5 @@
+CREATE TABLE IF NOT EXISTS users ( id BIGINT PRIMARY KEY, name VARCHAR(255), email VARCHAR(255) );
 INSERT INTO users (id, name, email)
 VALUES (1, 'John Doe', 'john.doe@example.com'),
        (2, 'Jane Smith', 'jane.smith@example.com'),
-       (3, 'Bob Johnson', 'bob.johnson@example.com') ON CONFLICT (email) DO NOTHING;
+       (3, 'Bob Johnson', 'bob.johnson@example.com') ON CONFLICT DO NOTHING;
