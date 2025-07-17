568a4c68ee04d554d4345fcfdc2733d65c6e1e07
Radman Hayati <Radman.hayatii@gmail.com>
2025-07-17 04:28:54 +0330

    fix: docker file fixed

--- src/main/java/com/radmanhayati/user/api/rest/user/mapper/UserResourceMapper.java
+++ src/main/java/com/radmanhayati/user/api/rest/user/mapper/UserResourceMapper.java
@@ -1,10 +1,12 @@
 package com.radmanhayati.user.api.rest.user.mapper;
 
 import com.radmanhayati.user.api.rest.user.model.request.RegisterUserRequest;
+import com.radmanhayati.user.api.rest.user.model.response.UserDto;
 import com.radmanhayati.user.api.rest.user.model.response.UserResponse;
 import com.radmanhayati.user.api.rest.user.model.response.UsersResponse;
 import com.radmanhayati.user.common.ResultStatus;
 import com.radmanhayati.user.service.user.model.AddUserModel;
+import com.radmanhayati.user.service.user.model.UserInfo;
 import com.radmanhayati.user.service.user.model.UserResult;
 import com.radmanhayati.user.service.user.model.UsersResult;
 import org.mapstruct.Mapper;
@@ -15,6 +17,11 @@ public interface UserResourceMapper {
 	@Mapping(target = "users", source = "users")
 	@Mapping(target = "result", expression = "java(ResultStatus.SUCCESS)")
 	UsersResponse toUsersResponse(UsersResult result);
+
+	@Mapping(target = "name", source = "name")
+	@Mapping(target = "email", source = "email")
+	@Mapping(target = "id", source = "id")
+	UserDto toUserDto(UserInfo user);
 
 	@Mapping(target = "user", source = "user")
 	@Mapping(target = "result", expression = "java(ResultStatus.SUCCESS)")
--- src/main/java/com/radmanhayati/user/api/rest/user/model/response/UserDto.java
+++ src/main/java/com/radmanhayati/user/api/rest/user/model/response/UserDto.java
@@ -9,7 +9,7 @@ import lombok.ToString;
 @ToString
 public class UserDto {
 
-	private Long id;
+	private String id;
 
 	private String name;
 
